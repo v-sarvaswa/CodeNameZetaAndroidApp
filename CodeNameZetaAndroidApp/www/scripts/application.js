8d16bd1dc01c9f909e13cc1bd20e48c48dadcd27
v-sarvaswa <venkzz@sarvaswa.com>
2017-09-09 15:48:34 +0530

    Status Bar Plugin

--- CodeNameZetaAndroidApp/www/scripts/application.js
+++ CodeNameZetaAndroidApp/www/scripts/application.js
@@ -13,6 +13,11 @@ define(["require", "exports"], function (require, exports) {
         document.addEventListener('pause', onPause, false);
         document.addEventListener('resume', onResume, false);
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
     }
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
