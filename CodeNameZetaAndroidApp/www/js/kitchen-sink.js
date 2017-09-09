871f78aba7294c43a7eb44d31fcf9898ab82cdcf
v-sarvaswa <venkzz@sarvaswa.com>
2017-09-09 15:14:28 +0530

    Intro Slides R3

--- CodeNameZetaAndroidApp/www/js/kitchen-sink.js
+++ CodeNameZetaAndroidApp/www/js/kitchen-sink.js
@@ -12,9 +12,9 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
 });
 // Add another view, which is in right panel
-var rightView = myApp.addView('.view-right', {
+/*var rightView = myApp.addView('.view-right', {
     name: 'right'
-});
+});*/
 
 // Show/hide preloader for remote ajax loaded pages
 // Probably should be removed on a production/local app
