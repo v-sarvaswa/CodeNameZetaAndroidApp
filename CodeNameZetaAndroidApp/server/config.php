bb963c8e02f724decf8a8f35d8f71c12280d8c95
v-sarvaswa <venkzz@sarvaswa.com>
2017-09-12 23:33:31 +0530

    Neuron Revamped [R1] & PHPs Renamed

--- CodeNameZetaAndroidApp/server/config.php
+++ CodeNameZetaAndroidApp/server/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DBHOST', 'localhost'); //111.118.215.222
+define('DBHOST', 'localhost');
 define('DBUSER', 'stylokzt_alpha');
 define('DBPASS', 'sarvaswa');
 define('DBNAME', 'stylokzt_uniportal');
@@ -11,14 +11,12 @@ printf("Can't connect to MySQL Server.", mysqli_connect_error());
 exit;
 }
 
-
 $connect2= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
 if (!$connect2)
 {
 printf("Can't connect to MySQL Server.", mysqli_connect_error());
 exit;
 }
-
 
 $connect3= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
 if (!$connect3)
