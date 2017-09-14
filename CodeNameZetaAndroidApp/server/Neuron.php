474874310c068e4125175467d8e41c21272f28e1
v-sarvaswa <venkzz@sarvaswa.com>
2017-09-14 15:44:55 +0530

    Neuron Updated [R4]

--- CodeNameZetaAndroidApp/server/Neuron.php
+++ CodeNameZetaAndroidApp/server/Neuron.php
@@ -1,61 +1,110 @@
 <?php header('Access-Control-Allow-Origin: *');?>
-<?php 
+<?php
 	require_once('config.php');
 	require_once('mail.php');
 
 	//Drop Point - Handle Requests from Synapse (Defaults to no_trigger_received & neuron)
+    $usertype = $_POST["usertype"] ?? 'neuron';
 	$trigger = $_POST["trigger"] ?? 'no_trigger_received';
-	$usertype = $_POST["usertype"] ?? 'neuron';
 	$request_instance = new $usertype();
 	$request_instance->$trigger();
-	
+
 	//Neuron Class
 	class neuron
 	{
 		private $sendresponse_url;
+        private $conn;
 
-		function __construct()
+		public function __construct()
 		{
-			$sendresponse_url = "return.php?data=";
+			$this->sendresponse_url = "return.php?data=";
+            $this->conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+            if ($this->conn->connect_error)
+            {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
 		}
 
 		function no_trigger_received()
 		{
-			sendresponse('no_trigger_received');
+			sendResponse('no_trigger_received');
 		}
-		
-		function sendresponse($data)
+
+		function sendResponse($data)
 		{
 			Header("Location: $this->sendresponse_url".$data);
 		}
+
+        function callSPIUD()
+        {
+            $ct = func_num_args();
+            $param = '';
+            for ($i=1; $i<$ct; $i++)
+            {
+                if($i==$ct-1)
+                    $param .= "'" . func_get_arg($i) . "'";
+                else
+                    $param .= "'" . func_get_arg($i) . "',";
+            }
+            $qr = mysqli_query($this->conn,"CALL ". func_get_arg(0) . "($param)");
+            $this->conn->close();
+            return $qr;
+        }
+
+        function callSPRead()
+        {
+            $ct = func_num_args();
+            $param = '';
+            for ($i=1; $i<$ct; $i++)
+            {
+                if($i==$ct-1)
+                    $param .= "'" . func_get_arg($i) . "'";
+                else
+                    $param .= "'" . func_get_arg($i) . "',";
+            }
+            $qr = $this->conn->query("CALL ". func_get_arg(0) . "($param)");
+            return $qr;
+        }
 	}
+
 	class enduser extends neuron
 	{
-		function __construct()
+		public function __construct()
 		{
-		
+            parent::__construct();
 		}
 	}
+
 	class admin extends neuron
 	{
-		function __construct()
+		public function __construct()
 		{
-		
+            parent::__construct();
 		}
 	}
+
 	class proposer extends neuron
 	{
-		function __construct()
+		public function __construct()
 		{
-		
+		    parent::__construct();
 		}
 	}
+
 	class portal extends neuron
 	{
-		function __construct()
+		public function __construct()
 		{
-		
+			parent::__construct();
 		}
+
+        function trial()
+        {
+            $result = parent::callSPRead('e_selectall',24);
+            while ($row = $result->fetch_object())
+            {
+                print_r($row->value);
+            }
+        }
 	}
-	
 ?>
--- CodeNameZetaAndroidApp/server/config.php
+++ CodeNameZetaAndroidApp/server/config.php
@@ -2,26 +2,12 @@
 define('DBHOST', 'localhost');
 define('DBUSER', 'stylokzt_alpha');
 define('DBPASS', 'sarvaswa');
-define('DBNAME', 'stylokzt_uniportal');
+define('DBNAME', 'stylokzt_temp_uniportal');
 
-$connect= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
+/*$connect= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
 if(!$connect)
 {
 printf("Can't connect to MySQL Server.", mysqli_connect_error());
 exit;
-}
-
-$connect2= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
-if (!$connect2)
-{
-printf("Can't connect to MySQL Server.", mysqli_connect_error());
-exit;
-}
-
-$connect3= mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
-if (!$connect3)
-{
-printf("Can't connect to MySQL Server.", mysqli_connect_error());
-exit;
-}
+}*/
 ?>
