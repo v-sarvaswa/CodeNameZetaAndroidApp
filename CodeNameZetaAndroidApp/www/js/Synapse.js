41c1d0021e574319fe8e4a1a5cf7dc5d59b79b4e
Dhanashri <Dhanashri@DHANASHRI-NB>
2017-09-21 15:27:12 +0530

    jquery validation testing

--- CodeNameZetaAndroidApp/www/js/Synapse.js
+++ CodeNameZetaAndroidApp/www/js/Synapse.js
@@ -22,7 +22,9 @@ function ajax(datatype, neuron, parameters, callback, error) {
         error: error
     })
 }
-function isEmail(email) {
+function isEmail(email)
+{
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
+
