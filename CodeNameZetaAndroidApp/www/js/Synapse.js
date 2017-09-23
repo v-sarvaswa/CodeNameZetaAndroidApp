0a1b57d8198aebaf2203d1c9118a8aeb52c9214f
v-sarvaswa <venkzz@sarvaswa.com>
2017-09-23 16:10:53 +0530

    Synapse Updated for SSO

--- CodeNameZetaAndroidApp/www/js/Synapse.js
+++ CodeNameZetaAndroidApp/www/js/Synapse.js
@@ -72,3 +72,46 @@ function passwordCheck(pwd, confirmpwd)
     return expr;
 }
 
+function trySilentLogin() {
+    window.plugins.googleplus.trySilentLogin(
+        {},
+        function (obj) {
+            document.querySelector("#feedback").innerHTML = "Silent hi, " + obj.displayName + ", " + obj.email;
+        },
+        function (msg) {
+            document.querySelector("#feedback").innerHTML = "error: " + msg;
+        }
+    );
+}
+
+function logout() {
+    window.plugins.googleplus.logout(
+        function (msg) {
+            document.querySelector("#image").style.visibility = 'hidden';
+            document.querySelector("#feedback").innerHTML = msg;
+        },
+        function (msg) {
+            document.querySelector("#feedback").innerHTML = msg;
+        }
+    );
+}
+
+function disconnect() {
+    window.plugins.googleplus.disconnect(
+        function (msg) {
+            document.querySelector("#image").style.visibility = 'hidden';
+            document.querySelector("#feedback").innerHTML = msg;
+        },
+        function (msg) {
+            document.querySelector("#feedback").innerHTML = msg;
+        }
+    );
+}
+
+window.onerror = function (what, line, file) {
+    alert(what + '; ' + line + '; ' + file);
+};
+
+function handleOpenURL(url) {
+    document.querySelector("#feedback").innerHTML = "App was opened by URL: " + url;
+}
